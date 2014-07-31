c96907e65a3e9a7e15b4189b1d287b2baf7e34f7
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-31 05:46:35 -0500

    first test passes

--- test/lib/dew_test.js
+++ test/lib/dew_test.js
@@ -1,12 +1,18 @@
-Dew = require('../../lib/dew.js')
+var expect = require('chai').expect;
+
 describe("Dew", function() {
+  var Dew = require('../../lib/dew.js');
   var dew = null;
   beforeEach(function() {
-    d
+    dew = new Dew();
   });
   describe("list()", function() {
+    var out = null;
+    beforeEach(function() {
+      out = dew.list();
+    });
     it("returns available drops", function() {
-      
+      expect(out).to.deep.eq([]);
     });
   });
 });
