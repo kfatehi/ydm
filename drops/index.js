04e4ea90948aacb9b1fd1c79cad055010eb98c61
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-31 10:59:56 -0500

    simplify

---
+++ drops/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  gitlab: require('./gitlab'),
+  postgresql: require('./postgresql'),
+  strider: require('./strider')
+}
--- test/lib/dew_test.js
+++ test/lib/dew_test.js
@@ -1,45 +1,36 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
 
 describe("Dew", function() {
   var Dew = require('../../lib/dew.js');
   var dew = null;
-  var out = null;
 
   beforeEach(function() {
-    dew = new Dew({
-      src: require('path').resolve('.')
-    });
-  });
-
-  describe("drops()", function() {
-    beforeEach(function(done) {
-      dew.drops(function (err, res) {
-        if (err) throw err;
-        out = res;
-        done();
-      });
-    });
-
-    it("returns available drops", function() {
-      expect(Object.keys(out)).
-        to.contain('example').and.
-        to.contain('example2');
-    });
+    dew = new Dew();
   });
 
   describe("install()", function() {
     describe("invalid drop", function() {
-      it("raises an error", function(done) {
-        dew.install('invalid', function (err, res) {
-          expect(err.message).to.match(/No such drop/)
-          expect(res).not.to.be.ok
-          done()
-        });
+      it("raises an error", function() {
+        expect(function () {
+          dew.install('invalid'); 
+        }).to.throw(Error);
       });
     });
 
-    it("", function() {
-      
+    describe("valid drop", function() {
+      var drop = null;
+      beforeEach(function() {
+        drop = require('../../drops/gitlab');
+        sinon.stub(drop, 'install');
+      });
+      afterEach(function() {
+        drop.install.restore();     
+      });
+      it("calls its install() function", function() {
+        dew.install('gitlab')
+        expect(drop.install.callCount).to.eq(1);
+      });
     });
   });
 });
