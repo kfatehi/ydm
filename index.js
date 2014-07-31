416e406115ba3e8775f047be0591463ffd2838bb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-31 05:35:55 -0500

    pushy

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  Dew: require('./lib/dew.js')
+}
---
+++ test/lib/dew_test.js
@@ -0,0 +1,12 @@
+Dew = require('../../lib/dew.js')
+describe("Dew", function() {
+  var dew = null;
+  beforeEach(function() {
+    d
+  });
+  describe("list()", function() {
+    it("returns available drops", function() {
+      
+    });
+  });
+});
