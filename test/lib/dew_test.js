28acec68289f2c5adcfdbeb80e50eb9f49ac3274
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-31 10:15:24 -0500

    progressin

--- test/lib/dew_test.js
+++ test/lib/dew_test.js
@@ -3,13 +3,15 @@ var expect = require('chai').expect;
 describe("Dew", function() {
   var Dew = require('../../lib/dew.js');
   var dew = null;
+  var out = null;
+
   beforeEach(function() {
     dew = new Dew({
       src: require('path').resolve('.')
     });
   });
+
   describe("drops()", function() {
-    var out = null;
     beforeEach(function(done) {
       dew.drops(function (err, res) {
         if (err) throw err;
@@ -17,10 +19,27 @@ describe("Dew", function() {
         done();
       });
     });
+
     it("returns available drops", function() {
       expect(Object.keys(out)).
         to.contain('example').and.
         to.contain('example2');
+    });
+  });
+
+  describe("install()", function() {
+    describe("invalid drop", function() {
+      it("raises an error", function(done) {
+        dew.install('invalid', function (err, res) {
+          expect(err.message).to.match(/No such drop/)
+          expect(res).not.to.be.ok
+          done()
+        });
+      });
+    });
+
+    it("", function() {
+      
     });
   });
 });
