e58623628531c432691570a18b45161d03818912
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-01 18:31:07 -0400

    implement and use tailUntilMatch method

--- drops/postgresql.js
+++ drops/postgresql.js
@@ -7,31 +7,13 @@ module.exports = function Postgresql(scope) {
         data: '/var/lib/postgresql'
       }
     }, function (err) {
-      var inspect = require('util').inspect;
       if (err) throw err;
-      scope.container.logs({
-        follow: true,
-        stdout: true,
-        stderr: true,
-        tail: 10
-      }, function (err, stream) {
+      scope.tailUntilMatch(/User:\s(\S+),\sPassword:\s(\S+)\s/, function (err, match) {
         if (err) throw err;
-        stream.on('end', function() { done(err) });
-        stream.on('error', function(e2) { err = new Error(e2) });
-        stream.on('data', function (chunk) {
-          var string = chunk.toString('utf-8');
-          var match = string.match(/User:\s(\S+),\sPassword:\s(\S+)\s/)
-          if (match) {
-            stream.destroy(null)
-            scope.data.info = {
-              user: match[1],
-              password: match[2]
-            }
-            scope.save()
-            done(null, scope.data.info)
-          }
-        });
-      })
+        scope.data.info = { user: match[1], password: match[2] }
+        scope.save()
+        done(null, scope.data.info)
+      });
     });
   }
 
