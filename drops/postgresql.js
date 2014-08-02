ff2a64095f69549be5e243a49a4145b39bcd9769
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-02 19:39:55 -0400

    cleanup

--- drops/postgresql.js
+++ drops/postgresql.js
@@ -8,10 +8,12 @@ module.exports = function(scope) {
         }
       }, function (err) {
         if (err) done(new Error(err))
-        scope.tailUntilMatch(/User:\s(\S+),\sPassword:\s(\S+)\s/, function (err, match) {
+        scope.tailUntilMatch(/User:\s(\S+),\sPassword:\s(\S+)\s/, function (err, string, user, pass) {
           if (err) done(new Error(err))
-          scope.localStorage.setItem('pg_user', match[1])
-          scope.localStorage.setItem('pg_password', match[2])
+          scope.localStorage.setItem('pg_user', user)
+          scope.localStorage.setItem('pg_pass', pass);
+          console.log(scope.name+".pg_user: "+user);
+          console.log(scope.name+".pg_pass: "+pass);
           done(null)
         });
       });
