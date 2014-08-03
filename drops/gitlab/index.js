697eb6651d9c61db580104d3638998170f75379d
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-02 20:05:00 -0400

    get ip address into gitlab env

--- drops/gitlab/index.js
+++ drops/gitlab/index.js
@@ -1,19 +1,32 @@
 module.exports = function(scope, argv, dew) {
   var PostgreSQL = dew.drops['postgresql'](argv, dew);
-  var pg = new PostgreSQL()
+  var env = {
+    SMTP_DOMAIN: 'knban.com',
+    SMTP_HOST: 'localhost',
+    SMTP_PORT: 25,
+    GITLAB_HTTPS: true,
+    GITLAB_HTTPS_ONLY: false,
+    GITLAB_HOST: 'gitlab.knban.com',
+    GITLAB_EMAIL: 'gitlab@knban.com',
+    DB_TYPE: 'postgres',
+    DB_HOST: null,
+    DB_USER: 'gitlab',
+    DB_PASS: 'password',
+    DB_NAME: 'gitlabhq_production'
+  }
   return {
     install: function (done) {
+      var pg = new PostgreSQL()
       pg.install(function (err, pg_user, pg_pass) {
         if (err) throw new Error(err);
-
-      });
-    }
-  }
-}
-
-/*
-        if (err) throw err;
+        pg.inspect(function (err, data) {
+          env.DB_HOST = data.NetworkSettings.IPAddress;
+          console.log(env);
+          
+        });
+        /*
         scope.applyConfig({
+          image: 
           image: "sameersbn/gitlab:7.1.1",
           ports: {
             10022: 22,
@@ -26,21 +39,12 @@ module.exports = function(scope, argv, dew) {
             postgresql: 'postgresql'
           },
           env: {
-            SMTP_DOMAIN: 'knban.com',
-            SMTP_HOST: 'localhost',
-            SMTP_PORT: 25,
-            GITLAB_HTTPS: true,
-            GITLAB_HTTPS_ONLY: false,
-            GITLAB_HOST: 'gitlab.knban.com',
-            GITLAB_EMAIL: 'gitlab@knban.com',
-            DB_TYPE: 'postgres',
-            DB_HOST: pg.data('host'),
-            DB_USER: 'gitlab',
-            DB_PASS: 'password',
-            DB_NAME: 'gitlabhq_production'
           }
         }, function (err) {
           if (err) throw err;
           scope.tailForever();
-        });
-       */
+        })*/
+      });
+    }
+  }
+}
