0a250f615c197d70157e0f1a3ad6fcb638d4edd4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-05 11:35:47 -0500

    more testing

--- test/lib/state_test.js
+++ test/lib/state_test.js
@@ -5,12 +5,44 @@ describe('State', function() {
 
   beforeEach(function() {
     scope = helper.buildScope('state-tests', { namespace: "dewey" })
-    state = scope.state;
+    state = scope.state
+  });
+
+  it("exposes the docker connection", function() {
+    expect(state.dockerConnection).to.be.ok;
   });
 
   describe("getContainer()", function() {
     it("is a function", function() {
       expect(state.getContainer).to.be.an.instanceof(Function)
+    });
+  });
+
+  describe("pullImage()", function() {
+    var mock = null;
+    beforeEach(function() {
+      mock = helper.mocker().post('/images/create?fromImage=some-proggie&tag=0.0.1', {
+        "fromImage": "some-proggie",
+        "tag": "0.0.1"
+      })
+    });
+    describe("when there is an error", function() {
+      it("calls back with the error", function(done) {
+        mock.reply(200, '{"error": "the error"}')
+        state.pullImage('some-proggie:0.0.1', function (err) {
+          expect(err.message).to.match(/the error/)
+          done();
+        })
+      });
+    });
+    describe("when everything looks ok", function() {
+      it("calls back with null error", function(done) {
+        mock.reply(200, '{"cool": "all good"}')
+        state.pullImage('some-proggie:0.0.1', function (err) {
+          expect(err).to.eq(null)
+          done();
+        })
+      });
     });
   });
 
@@ -89,7 +121,24 @@ describe('State', function() {
         });
       });
     });
+
+    describe("mocking docker to 200 on Container#inspect", function() {
+      beforeEach(function() {
+        scope.storage.setItem('_id', 1);
+        helper.mocker().get('/containers/1/json').reply(200, {
+          HereIs: "your Shit"
+        })
+      });
+      it("calls State#ensure() once with a callback", function(done) {
+        sinon.stub(state, 'ensure').yields()
+        state.apply(scope, { create: {} }, function (err, res) {
+          expect(state.ensure.callCount).to.eq(1)
+          expect(state.ensure.getCall(0).args[0]).to.be.an.instanceof(Function)
+          state.ensure.restore()
+          done();
+        })
+      });
+    });
+
   });
-
-
 });
