95bcde0d19028def3ef89da8d2ace909bd3e17c6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-16 15:00:22 -0700

    finish rename

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-var Dew = require('./lib/dew')
-  , Scope = require('./lib/scope')
-  , scopeMaker = require('./lib/scope_maker')
-
-module.exports = {
-  Dew: Dew,
-  Scope: Scope,
-  buildDrop: dropMaker,
-  findOrCreateScope: scopeMaker
-}
+module.exports = require('./lib/app')
